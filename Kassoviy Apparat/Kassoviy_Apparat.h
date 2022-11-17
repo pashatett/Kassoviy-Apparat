a621a7efeb1737a85970621156150bfda0810471
pashka <100860878+pashatett@users.noreply.github.com>
2022-11-17 20:53:24 +0300

    Update Kassoviy_Apparat.h

--- Kassoviy Apparat/Kassoviy_Apparat.h
+++ Kassoviy Apparat/Kassoviy_Apparat.h
@@ -4,11 +4,8 @@ using namespace std;
 class Kassoviy_Apparat{
 public:
 	void tovari();
-	void tovar_zero();
-	void delete_tovar(char* delete_tovar);
 	void all_viruchka();
 	void check();
 	void buy_tovar();
 	void people();
 };
-
